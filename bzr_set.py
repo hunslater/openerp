a2f12b6d9428cd724c544d94062ce3759ff988be
Fabien Pinckaers <fp@tinyerp.com>
2008-08-15 10:13:52 +0200

    Modifs

--- bzr_set.py
+++ bzr_set.py
@@ -7,8 +7,8 @@ bzr_repository = {
 	'server': 'bzr+ssh://fp-tinyerp@bazaar.launchpad.net/~openerp/openobject-server/trunk',
 	'client': 'bzr+ssh://fp-tinyerp@bazaar.launchpad.net/~openerp/openobject-client/trunk',
 	'addons': 'bzr+ssh://fp-tinyerp@bazaar.launchpad.net/~openerp/openobject-addons/trunk',
-#	'web': 'bzr+ssh://fp-tinyerp@bazaar.launchpad.net/~openerp/openobject-client-web/trunk',
-#	'addons-extra': 'bzr+ssh://fp-tinyerp@bazaar.launchpad.net/~openerp/openobject-addons/trunk-extra-addons',
+	'web': 'bzr+ssh://fp-tinyerp@bazaar.launchpad.net/~openerp/openobject-client-web/trunk',
+	'addons-extra': 'bzr+ssh://fp-tinyerp@bazaar.launchpad.net/~openerp/openobject-addons/trunk-extra-addons',
 }
 
 # Subscribe here links to modules you are interrested in
@@ -16,23 +16,6 @@ bzr_repository = {
 bzr_links = {
 	'addons/*': 'server/bin/addons/',
 }
-
-#bzr_links = {
-#	'addons/base_setup': 'server/bin/addons/',
-#	'addons/product': 'server/bin/addons/',
-#	'addons/hr': 'server/bin/addons/',
-#	'addons/board': 'server/bin/addons/',
-#	'addons/crm': 'server/bin/addons/',
-#	'addons/account': 'server/bin/addons/',
-#	'addons/stock': 'server/bin/addons/',
-#	'addons/purchase': 'server/bin/addons/',
-#	'addons/mrp': 'server/bin/addons/',
-#	'addons/report_mrp': 'server/bin/addons/',
-#	'addons/sale': 'server/bin/addons/',
-#	'addons/board_manufacturing': 'server/bin/addons/',
-#	'addons/delivery': 'server/bin/addons/',
-#	'addons/profile_manufacturing': 'server/bin/addons/',
-#}
 
 # ---------------------- End of Project Configuration -------------
 
@@ -46,14 +29,15 @@ import optparse
 from bzrlib.builtins import cmd_checkout, cmd_branch
 
 for local,bzrdir in bzr_repository.items():
-	print 'Checkout', bzrdir
-	# TODO: improve this using a bzr call if possible
+	print 'Branching', bzrdir
 	if not os.path.isdir(os.path.join(local,'.bzr')):
 		#cmd = cmd_checkout()
 		#cmd.run(bzrdir, local, lightweight=True)
 		cmd = cmd_branch()
 		cmd.run(bzrdir, local)
 	file(os.path.join(local,'.bzrignore'), 'wb+').write('*.pyc\n.svn\n.bzrignore\n')
+
+# Doing symlinks
 
 for src2,dest2 in bzr_links.items():
 	for src in glob.glob(src2):
@@ -64,3 +48,4 @@ for src2,dest2 in bzr_links.items():
 			if dest.startswith(local):
 				file(os.path.join(local,'.bzrignore'), 'ab+').write(dest[len(local):]+'\n')
 
+print
