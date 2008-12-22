092aff5ffbddf41fb2effcccaf7f4852a97d7b31
Christophe Simonis <christophe@cobalt>
2008-12-22 22:38:43 +0100

    [IMP] the script can now update working directories

--- bzr_set.py
+++ bzr_set.py
@@ -16,26 +16,26 @@ dest_dir = args and args[0] or '.'
 branch = opt.lplogin is None
 
 if branch:
-	BASEURL = 'lp:'	
+    BASEURL = 'lp:'    
 else:
-	BASEURL = 'bzr+ssh://%s@bazaar.launchpad.net/' % (opt.lplogin,)
+    BASEURL = 'bzr+ssh://%s@bazaar.launchpad.net/' % (opt.lplogin,)
 
 bzr_repository = {
-	'server': BASEURL + '~openerp/openobject-server/' + opt.version,
-	'client': BASEURL + '~openerp/openobject-client/' + opt.version,
-	'addons': BASEURL + '~openerp/openobject-addons/' + opt.version,
-	'addons-extra': BASEURL + '~openerp-commiter/openobject-addons/' + opt.version + '-extra-addons',
-	'web': BASEURL + '~openerp/openobject-client-web/' + opt.version,
+    'server': BASEURL + '~openerp/openobject-server/' + opt.version,
+    'client': BASEURL + '~openerp/openobject-client/' + opt.version,
+    'addons': BASEURL + '~openerp/openobject-addons/' + opt.version,
+    'addons-extra': BASEURL + '~openerp-commiter/openobject-addons/' + opt.version + '-extra-addons',
+    'web': BASEURL + '~openerp/openobject-client-web/' + opt.version,
 }
 
 # Subscribe here links to modules you are interrested in
 bzr_links = {
-	'addons/*': 'server/bin/addons/',
+    'addons/*': 'server/bin/addons/',
 }
 
 if opt.bi and opt.version == "trunk":
-	bzr_repository['bi'] = BASEURL + '~openerp/openobject-bi/' + opt.version
-	bzr_links['bi/addons/*'] = 'server/bin/addons/'
+    bzr_repository['bi'] = BASEURL + '~openerp/openobject-bi/' + opt.version
+    bzr_links['bi/addons/*'] = 'server/bin/addons/'
 
 # ---------------------- End of Project Configuration -------------
 
@@ -45,40 +45,64 @@ if opt.bi and opt.version == "trunk":
 
 import os
 import glob
-from bzrlib.builtins import cmd_checkout, cmd_branch
+import bzrlib.builtins
 from bzrlib.plugins import launchpad
+from bzrlib.branch import Branch
+from bzrlib.errors import NotBranchError
+
+def run_cmd(cmdname, *args, **kwargs):
+    f = getattr(bzrlib.builtins, 'cmd_' + cmdname)()
+    if hasattr(f, '_setup_outf'):
+        # old versions of bzr does not have this function   
+        # this function must be called to avoid a exception in bzr code
+        f._setup_outf()
+    return f.run(*args, **kwargs)
 
 if branch:
-	cmd = cmd_branch()
-	cmd_args = {}
+    cmd = {'new': lambda u, l: run_cmd('branch', u, l),
+           'update': lambda u, l: run_cmd('pull', u, directory=l, overwrite=True),
+    }
 else:
-	cmd = cmd_checkout()
-	cmd_args = {'lightweight':True}
+    cmd = {'new': lambda u, l: run_cmd('checkout', u, l, lightweight=True),
+           'update': lambda u, l: run_cmd('update', l),
+    }
+
+msg = "%(status)s %(type)s %(to)s from %(from)s"
+
+if not os.path.exists(dest_dir):
+    os.makedirs(dest_dir)
 
 for local,bzrdir in bzr_repository.items():
-	local = os.path.join(dest_dir, local)
-	print branch and 'Branch' or 'Checkout', bzrdir, 'to' ,local
-	# TODO: improve this using a bzr call if possible
-	if not os.path.isdir(os.path.join(local,'.bzr')):
-		cmd.run(bzrdir, local, **cmd_args)
-	file(os.path.join(local,'.bzrignore'), 'wb+').write('*.pyc\n.*.swp\n.bzrignore\n')
+    local = os.path.join(dest_dir, local)
+    
+    try:
+        Branch.open(local)
+        # FIXME check that the current workingDirectory is a branch or a checkout
+        status = 'update'
+    except NotBranchError:
+        status = 'new'
+    
+    print msg % {'status': status, 'type': ['checkout', 'branch'][branch], 'to': local, 'from': bzrdir}
+            
+    cmd[status](bzrdir, local)
 
 # Doing symlinks
 
 print '(Re)Computing Symbolic links...'
 for src2,dest2 in bzr_links.items():
-	src2 = os.path.join(dest_dir, src2)
-	dest2 = os.path.join(dest_dir, dest2)
-	for src in glob.glob(src2):
-		dest = os.path.join(dest2, os.path.basename(src))
-		if not os.path.isdir(dest):
-			os.symlink(os.path.realpath(src), dest)
-		for local in bzr_repository:
-			if dest.startswith(local):
-				file(os.path.join(local,'.bzrignore'), 'ab+').write(dest[len(local):]+'\n')
+    src2 = os.path.join(dest_dir, src2)
+    dest2 = os.path.join(dest_dir, dest2)
+    for src in glob.glob(src2):
+        dest = os.path.join(dest2, os.path.basename(src))
+        if not os.path.isdir(dest):
+            os.symlink(os.path.realpath(src), dest)
+        for local in bzr_repository:
+            if dest.startswith(local):
+                file(os.path.join(local,'.bzrignore'), 'ab+').write(dest[len(local):]+'\n')
 
 print
 print 'Sources of OpenERP have been installed. If you develop new features,'
 print 'you can get more information on how to contribute to the project here:'
 print '\thttp://openerp.com/community-process.html'
 print
+
